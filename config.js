cc5bccb8a225afda17f90be06c47452c31ecc69e
Matheus Felix <139878742+MFMarinho@users.noreply.github.com>
2023-11-18 13:23:37 +0000

    instalando bibliotecas

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+      username: 'root', // Seu nome de usuário do banco de dados
+      password: 'root', // Sua senha do banco de dados
+      database: 'produto', // Nome do seu banco de dados
+      host: 'localhost',
+      dialect: 'mysql', // O banco de dados que você está usando
+    },
+    production: {},
+  };
